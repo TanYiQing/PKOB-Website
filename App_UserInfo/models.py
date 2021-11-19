16f50c5cb244761ee0f692e375d6d61b47c43676
TanYiQing <yiqingtan99@gmail.com>
2021-11-19 19:45:49 +0800

    update setting.py

--- App_UserInfo/models.py
+++ App_UserInfo/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model):
+    matric_no = models.CharField(primary_key=True, max_length=10, verbose_name='Matric Number')
+    name = models.CharField(max_length=255, verbose_name='Name')
--- App_UserInfo/views.py
+++ App_UserInfo/views.py
@@ -1,19 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from App_UserInfo.models import Student
+
 
 def student(request):
     return HttpResponse("Hello My Students")
 
+
 def victim(request):
     return render(request, 'App_UserInfo/victim.html')
 
+
 def penghulu(request):
-    name="tan yi qing"
-    position="Penghulu"
+    name = "tan yi qing"
+    position = "Penghulu"
     return render(request, 'App_UserInfo/penghulu.html', context={'name': name, 'position': position})
 
-def victims_report(request):
-    victim_list = ["Tan Jia Earn", "Ng Shen Meng", "Wong Fang Man"]
-    return render(request, 'App_UserInfo/victim_report.html', context={'victim_list': victim_list})
 
+def victims_report(request):
+    student_list = Student.objects.all()
+    return render(request, 'App_UserInfo/victim_report.html', context={'student_list': student_list})
